eefc525f5174048df03e98f6881dfca4467b2427
Antonia Atanasova <antoniyaata@gmail.com>
2021-04-15 10:32:12 +0300

    Formatted code

--- data-structures/linked-list.js
+++ data-structures/linked-list.js
@@ -52,40 +52,53 @@ class LinkedList {
   _find(value, test = this.test) {
     let current = this.head;
     let i = 0;
+
     while (current) {
       if (test(value, current.value, i, current)) {
         return current;
       }
+
       current = current.next;
       i++;
     }
+
     return null;
   }
 
   get(index) {
     const node = this._find(index, this.testIndex);
+
     if (!node) return null;
+
     return node.value;
   }
 
   delete(index) {
     if (index === 0) {
       const { head } = this;
+
       if (head) {
         this.head = head.next;
       } else {
         this.head = null;
       }
+
       this.length--;
+
       return head.value;
     }
 
     const node = this._find(index - 1, this.testIndex);
     const excise = node.next;
+
     if (!excise) return null;
+
     node.next = excise.next;
+
     if (!node.next.next) this.tail = node.next;
+
     this.length--;
+
     return excise.value;
   }
 
@@ -100,11 +113,14 @@ class LinkedList {
   serialize() {
     const ans = [];
     let current = this.head;
+
     if (!current) return ans;
+
     while (current) {
       ans.push(current.value);
       current = current.next;
     }
+
     return ans;
   }
 }
